55cf387ee3f808fd859b24ee0e645a73804013ce
Pedro Licio <amaralrj@gmail.com>
2013-06-09 12:50:07 -0300

    Adiciona cadastro de Área de Conhecimento.

--- src/br/com/puc/sispol/controller/AreaDeConhecimentoController.java
+++ src/br/com/puc/sispol/controller/AreaDeConhecimentoController.java
@@ -1,10 +1,15 @@
 package br.com.puc.sispol.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import br.com.puc.sispol.dao.AreaDeConhecimentoDAO;
-import br.com.puc.sispol.dao.TarefaDAO;
+import br.com.puc.sispol.modelo.AreaDeConhecimento;
 
 @Controller
 public class AreaDeConhecimentoController {
@@ -17,6 +22,23 @@ public class AreaDeConhecimentoController {
 	@RequestMapping("novaAreaDeConhecimento")
 	public String form() {
 		return "area_de_conhecimento/formulario";
+	}
+
+	@RequestMapping("adicionaAreaDeConhecimento")
+	public ModelAndView adiciona(@Valid AreaDeConhecimento areaDeConhecimento,
+			BindingResult result) {
+
+		ModelAndView mv = new ModelAndView("area_de_conhecimento/formulario");
+		// mv.addObject("tarefas", tarefas);
+
+		if (result.hasFieldErrors()) {
+			System.out.println("ERRO");
+			return mv;
+		}
+
+		dao.adiciona(areaDeConhecimento);
+		mv.addObject("sucesso", 1);
+		return mv;
 	}
 
 }
---
+++ src/br/com/puc/sispol/modelo/AreaDeConhecimento.java
@@ -0,0 +1,48 @@
+package br.com.puc.sispol.modelo;
+
+import java.util.Calendar;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.NotEmpty;
+import org.springframework.format.annotation.DateTimeFormat;
+
+public class AreaDeConhecimento {
+	private Long id;
+
+	@NotNull(message = "O Título deve ser preenchido.")
+	@Size(min = 5, message = "O Título deve ser preenchido.")
+	@NotEmpty(message = "O Título deve ser preenchido.")
+	private String titulo;
+	
+	@NotNull(message = "A Descrição deve ser preenchida.")
+	@NotEmpty(message = "O Descrição deve ser preenchido.")
+	private String descricao;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+
+}
