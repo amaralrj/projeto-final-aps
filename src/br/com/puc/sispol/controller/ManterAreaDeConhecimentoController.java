b14aad1842e778e705c5e6460d116e29dbd3119f
Pedro Licio <amaralrj@gmail.com>
2013-07-17 22:52:16 -0300

    adiciona a funcao consultar area de conhecimento.

--- src/br/com/puc/sispol/controller/ManterAreaDeConhecimentoController.java
+++ src/br/com/puc/sispol/controller/ManterAreaDeConhecimentoController.java
@@ -1,11 +1,13 @@
 package br.com.puc.sispol.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.puc.sispol.dao.AreaDeConhecimentoDAO;
@@ -28,7 +30,8 @@ public class ManterAreaDeConhecimentoController {
 	public ModelAndView adiciona(@Valid AreaDeConhecimento areaDeConhecimento,
 			BindingResult result) {
 
-		ModelAndView mv = new ModelAndView("manter_area_de_conhecimento/incluir_area_de_conhecimento");
+		ModelAndView mv = new ModelAndView(
+				"manter_area_de_conhecimento/incluir_area_de_conhecimento");
 		// mv.addObject("tarefas", tarefas);
 
 		if (result.hasFieldErrors()) {
@@ -37,6 +40,32 @@ public class ManterAreaDeConhecimentoController {
 
 		dao.adiciona(areaDeConhecimento);
 		mv.addObject("sucesso", 1);
+		return mv;
+	}
+
+	@RequestMapping("consultarAreaDeConhecimento")
+	public ModelAndView consulta(HttpServletRequest request) {
+
+		ModelAndView mv = new ModelAndView(
+				"manter_area_de_conhecimento/consultar_area_de_conhecimento");
+
+		if (request.getParameter("titulo") != null) {
+			List<AreaDeConhecimento> areasDeConhecimento = dao
+					.buscaPorTitulo(request.getParameter("titulo"));
+			mv.addObject("areasDeConhecimento", areasDeConhecimento);
+		}
+		return mv;
+	}
+	
+	@RequestMapping("detalharAreaDeConhecimento")
+	public ModelAndView detalha(HttpServletRequest request) {
+
+		ModelAndView mv = new ModelAndView(
+				"manter_area_de_conhecimento/detalhar_area_de_conhecimento");
+
+		if (request.getParameter("codAreaDeconhecimento") != null) {
+			mv.addObject("areaDeConhecimento", dao.busca( Long.parseLong(request.getParameter("codAreaDeconhecimento"),10)));
+		}
 		return mv;
 	}
 
--- src/br/com/puc/sispol/dao/AreaDeConhecimentoDAO.java
+++ src/br/com/puc/sispol/dao/AreaDeConhecimentoDAO.java
@@ -1,17 +1,14 @@
 package br.com.puc.sispol.dao;
 
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.List;
 
 import br.com.puc.sispol.ConnectionFactory;
 import br.com.puc.sispol.modelo.AreaDeConhecimento;
-import br.com.puc.sispol.modelo.Tarefa;
 
 public class AreaDeConhecimentoDAO {
 	private final Connection connection;
@@ -68,6 +65,61 @@ public class AreaDeConhecimentoDAO {
 		areaDeConhecimento.setTitulo(rs.getString("Titulo"));
 		areaDeConhecimento.setDescricao(rs.getString("Descricao"));
 		return areaDeConhecimento;
+	}
+
+	public AreaDeConhecimento busca(Long codigo) {
+		AreaDeConhecimento areaDeConhecimento = null;
+		// TODO Auto-generated method stub
+		if (codigo == null) {
+			throw new IllegalStateException("Codigo da area de conhecimento não deve ser nulo.");
+		}
+		try {
+			
+			PreparedStatement stmt = this.connection
+					.prepareStatement("select CodAreaDeConhecimento, Titulo, Descricao from AreaDeConhecimento where CodAreaDeConhecimento = ?");
+			stmt.setLong(1, codigo);
+			System.out.println(stmt);
+			ResultSet rs = stmt.executeQuery();
+
+			while (rs.next()) {
+				// adiciona a tarefa na lista
+				areaDeConhecimento = populaAreaDeConhecimento(rs);
+			}
+
+			rs.close();
+			stmt.close();
+
+			return areaDeConhecimento;
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public List<AreaDeConhecimento> buscaPorTitulo(String titulo) {
+		// TODO Auto-generated method stub
+		if (titulo == null) {
+			throw new IllegalStateException("Titulo da area de conhecimento não deve ser nulo.");
+		}
+		try {
+			List<AreaDeConhecimento> areasDeConhecimento = new ArrayList<AreaDeConhecimento>();
+			PreparedStatement stmt = this.connection
+					.prepareStatement("select CodAreaDeConhecimento, Titulo, Descricao from AreaDeConhecimento where lower(Titulo) like lower(trim(?))");
+			stmt.setString(1, "%"+titulo+"%");
+			System.out.println(stmt);
+			ResultSet rs = stmt.executeQuery();
+
+			while (rs.next()) {
+				// adiciona a tarefa na lista
+				areasDeConhecimento.add(populaAreaDeConhecimento(rs));
+			}
+
+			rs.close();
+			stmt.close();
+
+			return areasDeConhecimento;
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 }
